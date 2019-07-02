7d7c0057ffbe27e9e2b78c6ca1a25637e06553a1
Josh DeFamio <josh@fifthturtle.com>
2019-07-02 11:22:41 -0700

    updated server
    updated server so that it takes its own IP address

--- server.js
+++ server.js
@@ -15,16 +15,7 @@ const server = Hapi.server({
   }
 });
 // */
-const server = Hapi.server({
-  host: '192.168.2.130',
-  port: 8051,
-  routes: {
-    "cors" : true,
-    files: {
-        relativeTo: Path.join(__dirname, 'dist')
-    }
-  }
-});
+let server;
 
 const diamondAssets = require('./lib/diamondAssets');
 
@@ -45,104 +36,107 @@ async function getGame(game_pk)
 
 // Add the route  
 
-server.route({
+function setRoutes()
+{
+  server.route({
+      method: 'GET',
+      path: '/',
+      handler: (request, h) => {
+          return h.file('index.html');
+      }
+  });
+
+  server.route({
     method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-        return h.file('index.html');
+    path: '/hello',
+    handler: function (request, h) {
+
+      return 'Hello World!';
     }
-});
+  });
 
-server.route({
-  method: 'GET',
-  path: '/hello',
-  handler: function (request, h) {
+  server.route({
+      method: 'GET',
+      path: '/css/{file_name}',
+      handler: async function (request, h) {
+          let _path = Path.join('css', request.params.file_name);
+          return h.file(_path);
+          /*
+          let file = fs.readFileSync(_path);
+          return file;
+          // */
+      }
+  });
 
-    return 'Hello World!';
-  }
-});
+  server.route({
+      method: 'GET',
+      path: '/js/{file_name}',
+      handler: async function (request, h) {
+          let _path = Path.join('js', request.params.file_name);
+          return h.file(_path);
+          /*
+          let file = fs.readFileSync(_path);
+          return file;
+          // */
+      }
+  });
 
-server.route({
+  server.route({
+      method: 'GET',
+      path: '/img/{file_name}',
+      handler: async function (request, h) {
+          let _path = Path.join('img', request.params.file_name);
+          console.log("IMG", _path);
+          return h.file(_path);
+      }
+  });
+
+  server.route({
     method: 'GET',
-    path: '/css/{file_name}',
+    path: '/schedule/{sched_date}',
     handler: async function (request, h) {
-        let _path = Path.join('css', request.params.file_name);
-        return h.file(_path);
+      console.log('in schedule');
+      let schedule = require("./lib/schedule");
+      let date = require("./lib/date");
+      let req_date;
+      console.log(`request.params.sched_date: ${request.params.sched_date}`);
+      req_date = date.isoToDate(new Date(request.params.sched_date));
+          //TODO add optional flag 
+          console.log(`req_date: ${req_date}`);
+      const jvSched = await schedule.get(req_date).catch((err) => {console.log(err);})
+      return jvSched;
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/game/{game_pk}',
+    handler: async function (request, h) {
         /*
-        let file = fs.readFileSync(_path);
-        return file;
-        // */
+      console.log('in schedule');
+      let Game = require("./lib/Game");
+          //TODO add optional flag 
+      let myGame = new Game(request.params.game_pk, null);
+      let jvGame = { game_pk: request.params.game_pk};
+      jvGame["boxscore"] = await myGame.getBoxscore().catch((err) => {console.log(err);})
+      jvGame["linescore"] = await myGame.getLinescore().catch((err) => {console.log(err);})
+      return jvGame;
+      */
+    console.log('getting game');
+    return await getGame(request.params.game_pk);
     }
-});
+  });
 
-server.route({
+  server.route({
     method: 'GET',
-    path: '/js/{file_name}',
+    path: '/assetsearch/{game_pk}',
     handler: async function (request, h) {
-        let _path = Path.join('js', request.params.file_name);
-        return h.file(_path);
-        /*
-        let file = fs.readFileSync(_path);
-        return file;
-        // */
+      let data = await diamondAssets.get(request.params.game_pk).catch((err) => { console.log(err) });
+      console.log(data);
+      return data.associated_info;
     }
-});
-
-server.route({
-    method: 'GET',
-    path: '/img/{file_name}',
-    handler: async function (request, h) {
-        let _path = Path.join('img', request.params.file_name);
-        console.log("IMG", _path);
-        return h.file(_path);
-    }
-});
-
-server.route({
-  method: 'GET',
-  path: '/schedule/{sched_date}',
-  handler: async function (request, h) {
-    console.log('in schedule');
-    let schedule = require("./lib/schedule");
-    let date = require("./lib/date");
-    let req_date;
-    console.log(`request.params.sched_date: ${request.params.sched_date}`);
-    req_date = date.isoToDate(new Date(request.params.sched_date));
-        //TODO add optional flag 
-        console.log(`req_date: ${req_date}`);
-    const jvSched = await schedule.get(req_date).catch((err) => {console.log(err);})
-    return jvSched;
-  }
-});
-
-server.route({
-  method: 'GET',
-  path: '/game/{game_pk}',
-  handler: async function (request, h) {
-      /*
-    console.log('in schedule');
-    let Game = require("./lib/Game");
-        //TODO add optional flag 
-    let myGame = new Game(request.params.game_pk, null);
-    let jvGame = { game_pk: request.params.game_pk};
-    jvGame["boxscore"] = await myGame.getBoxscore().catch((err) => {console.log(err);})
-    jvGame["linescore"] = await myGame.getLinescore().catch((err) => {console.log(err);})
-    return jvGame;
-    */
-   console.log('getting game');
-   return await getGame(request.params.game_pk);
-  }
-});
-
-server.route({
-  method: 'GET',
-  path: '/assetsearch/{game_pk}',
-  handler: async function (request, h) {
-    let data = await diamondAssets.get(request.params.game_pk).catch((err) => { console.log(err) });
-    console.log(data);
-    return data.associated_info;
-  }
-})
+  })
+}
 
 
 // Start the server  
@@ -160,4 +154,20 @@ async function start () {
   console.log('Server running at:', server.info.uri);
 };
 
-start();
+
+
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+  console.log('addr: '+add);
+  server = Hapi.server({
+    host: add,
+    port: 8051,
+    routes: {
+      "cors" : true,
+      files: {
+          relativeTo: Path.join(__dirname, 'dist')
+      }
+    }
+  });
+  console.log(server.info.uri);
+  setRoutes();
+})
